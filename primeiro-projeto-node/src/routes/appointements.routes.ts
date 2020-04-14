6ba9f79e8250812e478049516a10d6b2e7a683e8
jordanjpmg <jordanjpmg@gmail.com>
2020-04-14 09:45:17 -0300

    Inteface CreateAppointmentDTO criada

--- primeiro-projeto-node/src/routes/appointements.routes.ts
+++ primeiro-projeto-node/src/routes/appointements.routes.ts
@@ -1,34 +1,36 @@
 import { Router } from 'express';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
 
-import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
 
-const appointments: Appointment[] = [];
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
 
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(appointment.date, parsedDate),
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
   );
 
   if (findAppointmentInSameDate) {
     return response.status(400).json({ message: 'Agendamento ja cadastrado' });
   }
 
-  const appointment = new Appointment(provider, parsedDate);
-
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   response.json(appointment);
 });
 
 appointmentsRouter.get('/', (request, response) => {
-  response.json(appointments);
+  const appointment = appointmentsRepository.all();
+  response.json(appointment);
 });
 
 export default appointmentsRouter;
