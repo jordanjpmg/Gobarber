3bc5dc99ffc6bbda3cf2f02b4de849a386b0814d
jordanjpmg <jordanjpmg@gmail.com>
2020-04-15 12:45:22 -0300

    Migration CreateAppointments criada

--- primeiro-projeto-node/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import appointmentsRouter from './appointements.routes';
-
-const routes = Router();
-
-routes.use('/appointments', appointmentsRouter);
-
-export default routes;
