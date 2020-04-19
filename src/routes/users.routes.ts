eab3d60615f2975e101e0bee6ac82ad4c010d610
jordanjpmg <jordanjpmg@gmail.com>
2020-04-19 18:43:08 -0300

    Update de avatar criado

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,6 +4,8 @@ import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
 
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
@@ -34,9 +36,21 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file);
+    try {
+      const updateUserAvatarService = new UpdateUserAvatarService();
 
-    return response.json({ ok: true });
-});
+      const user = await updateUserAvatarService.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ erro: err.message });
+    }
+  },
+);
 
 export default usersRouter;
